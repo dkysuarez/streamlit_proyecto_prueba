7adfffef91ee0405f7d5e071040d661b848b6786
dkysuarez <dkysuarez1@gmail.com>
2026-03-09 17:15:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,6 @@ from matplotlib import pyplot as plt, patches
 import seaborn as sns
 from baseball_field_viz import transform_coords, draw_field, spraychart
 
-
-st.write("Hola")
-st.header("Soy Ali")
-st.subheader("Analista de datos")
-
 st.set_page_config(page_title= "Visualizacion de Beisbol", layout="wide")
 st.title("Visualizacion de Beisbol")
 st.markdown("---")
@@ -63,6 +58,7 @@ if viz_type == "Zonas de strike":
      ax.set_ylabel("Posicion Vertical")
      ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
      st.pyplot(fig)
+     plt.close()
 
 
      with col2:
@@ -82,4 +78,51 @@ if viz_type == "Zonas de strike":
       ax_heat.set_ylabel("Posicion Vertical")
 
       st.pyplot(fig_heat)
+      plt.close()
+
+      #Mapa campo interactivo
+elif viz_type == "Mapa de Campo":
+    st.header("Mapa de Bateo")
+    st.markdown("Visualizacion de donde caen las bolas bateadas en el campo")
+
+    color_by = st.radio("Colorea por:", ['events', 'launch_speed', 'launch_angle'])
+    fig2,ax2 = plt.subplots(figsize = (12,10))
+    spraychart(ax2,batting_data,color_by = color_by, title = f"Distribucion de Bateo - Coloreado por "
+                                                             f"{color_by}")
+    st.pyplot(fig2)
+    plt.close()
+
+#Grafico de Tendencias
+elif viz_type == "Tendencias":
+    st.header("Tendencias y Estadisticas")
+    st.markdown("Analisis de tendencias en el rendimiento del bateador")
+
+    st.subheader("Evolucion del Velocidad de salida")
+
+    batting_data['game_date'] = pd.to_datetime(batting_data['game_date'])
+    daily_stats =  batting_data.groupby('game_date').agg({
+        'launch_speed': 'mean',
+        'launch_angle': 'mean',
+        'events' : 'count'
+    }).reset_index()
+
+    col1, col2 =  st.columns(2)
+    with col1:
+        fig_hist = px.histogram(batting_data, x='launch_speed', nbins = 30,
+                                title='Histograma de velocidad de Salida',
+                                labels = {'launch_speed': 'Velocidad (mph)'})
+        st.plotly_chart(fig_hist, use_cotainer_width = True)
+
+    with col2:
+        st.subheader("Velocidad vs Angulo de Salida")
+        fig_scatter = px.scatter(batting_data, x="launch_speed", y="launch_angle",
+                                 color = 'events', hover_data = ['events'],
+                                 title = 'Relacion Velocidad-Angulo',
+                                 labels = {'launch_speed': 'Velocidad (mph)',
+                                           'launch_angle': 'Angulo (grados)'})
+        st.plotly_chart(fig_scatter, use_container  = True)
+
+
+
+
 
